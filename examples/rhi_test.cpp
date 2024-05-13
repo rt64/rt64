5f80997e5cd47a0e68db3057856be729e99e211e
dcvz <david@dcvz.io>
2024-05-13 13:49:29 +0200

    Build statically when buiding examples

--- examples/rhi_test.cpp
+++ examples/rhi_test.cpp
@@ -6,20 +6,15 @@ namespace RT64 {
 }
 
 int main(int argc, char** argv) {
-    std::unique_ptr<RT64::RenderInterface> renderInterface;
+    std::unique_ptr<RT64::RenderInterface> renderInterface = RT64::CreateVulkanInterface();
 
-    // Create a render interface with the preferred backend.
+#ifdef _WIN32
+    // Windows only: Can also use D3D12.
     const bool useVulkan = true;
-    if (useVulkan) {
-        renderInterface = RT64::CreateVulkanInterface();
-    }
-    else {
-#ifdef _WIN64
+    if (!useVulkan) {
         renderInterface = RT64::CreateD3D12Interface();
-#else
-        assert(false);
-#endif
     }
+#endif
 
     // Execute a blocking test that creates a window and draws some geometry to test the render interface.
     RT64::RenderInterfaceTest(renderInterface.get());
