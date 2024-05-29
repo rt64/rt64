ace62149dbed96f1e4e53f9cfe8dbb31d39ad1ab
dcvz <david@dcvz.io>
2024-05-29 21:05:14 +0200

    Window attributes without top bar

--- src/common/rt64_apple.mm
+++ src/common/rt64_apple.mm
@@ -6,11 +6,11 @@
 
 void GetWindowAttributes(void* window, CocoaWindowAttributes *attributes) {
     NSWindow *nsWindow = (NSWindow *)window;
-    NSRect frame = [nsWindow frame];
-    attributes->x = frame.origin.x;
-    attributes->y = frame.origin.y;
-    attributes->width = frame.size.width;
-    attributes->height = frame.size.height;
+    NSRect contentFrame = [[nsWindow contentView] frame];
+    attributes->x = contentFrame.origin.x;
+    attributes->y = contentFrame.origin.y;
+    attributes->width = contentFrame.size.width;
+    attributes->height = contentFrame.size.height;
 }
 
 const char* GetHomeDirectory() {
