d20f57ceb5f28dae0e45e8e2e2a17a7c5742a742
David Chavez <david@dcvz.io>
2025-03-23 18:12:25 +0100

    fix(metal): Fixes two instances of bad releases

--- src/metal/rt64_metal.cpp
+++ src/metal/rt64_metal.cpp
@@ -2111,6 +2111,7 @@ namespace RT64 {
                 needsUpdate = i >= stateCache.lastVertexBuffers.size() || interfaceBuffer->mtl != stateCache.lastVertexBuffers[i] || newOffset != stateCache.lastVertexBufferOffsets[i] || newIndex != stateCache.lastVertexBufferIndices[i];
 
                 vertexBuffers[i] = interfaceBuffer->mtl;
+                vertexBuffers[i]->retain();
                 vertexBufferOffsets[i] = newOffset;
                 vertexBufferIndices[i] = newIndex;
             }
@@ -3098,7 +3099,6 @@ namespace RT64 {
         resolveTexturePipelineState->release();
         clearVertexFunction->release();
         clearColorFunction->release();
-        clearDepthFunction->release();
         clearDepthFunction->release();
         device->release();
         reusableBlitDescriptor->release();
