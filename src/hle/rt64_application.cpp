90a40cb3e68b30e0a88f9ed4ec74b80c2e57a517
Darío <dariosamo@gmail.com>
2025-03-23 22:53:15 -0300

    Switch to Vulkan on old NVIDIA driver. (#146)
    * Switch to Vulkan on old NVIDIA driver.
    
    * Adjust version.

--- src/hle/rt64_application.cpp
+++ src/hle/rt64_application.cpp
@@ -185,6 +185,15 @@ namespace RT64 {
                         usesHardwareResolve = false;
                     }
                 }
+
+                if (automaticGraphicsAPI) {
+                    // Driver 475.14 or below seems to have much better performance in Vulkan, almost twice as fast on a GT 730. The workaround is we switch to Vulkan.
+                    // The reason behind this performance difference is currently unknown and doesn't show up in newer drivers or hardware.
+                    const uint64_t UnderperformingNVIDIADriverD3D12 = 0x001e0000000e1d5a; // 475.14
+                    if (driverVersion <= UnderperformingNVIDIADriverD3D12) {
+                        forceVulkanForCompatibility = true;
+                    }
+                }
             }
         }
         else if (deviceVendor == RenderDeviceVendor::AMD) {
