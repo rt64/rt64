6eaca5f5251af2daef33867320084aae556a6d11
Darío <dariosamo@gmail.com>
2025-04-09 09:09:38 -0300

    Update Default Database version to V5

--- src/common/rt64_replacement_database.h
+++ src/common/rt64_replacement_database.h
@@ -44,7 +44,7 @@ namespace RT64 {
     struct ReplacementConfiguration {
         ReplacementAutoPath autoPath = ReplacementAutoPath::RT64;
         uint32_t configurationVersion = 2;
-        uint32_t hashVersion = 3;
+        uint32_t hashVersion = 5;
     };
 
     struct ReplacementHashes {
